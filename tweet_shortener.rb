bb853b62741088d0ca5d4d490c4ff2e94573c875
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-18 23:39:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,7 +35,8 @@ def bulk_tweet_shortener(array)
 end
 
 def selective_tweet_shortener(tweet)
-    if tweet.length > 140
+    if
+      tweet.length > 140
     word_substituter(tweet)
     else
       tweet.length <= 130
