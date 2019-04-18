56ac0796c1fd85e1286df0efaf1d63c3faafdd87
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-18 19:20:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def dictionary
+
+  substitutions =
+  {"hello" => "hi",
+   "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b",
+    "you" => "u", "at" => "@", "and" => "&"}
+
+end
+
+def word_substituter(tweet)
+  tweet.split(" ").collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+  end
+
+
+
+
+    #convert string to array
+    #iterate over every word in the array
+    #if a word matches a key replace it with the value
+    #put the array back into a string and return the new string
