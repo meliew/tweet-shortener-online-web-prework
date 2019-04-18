b6891d6665903806d9fb808ad6b284826a4a8907
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-18 23:51:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,7 @@ def selective_tweet_shortener(tweet)
 
 def shortened_tweet_truncator(tweet)
 if word_substituter(tweet).length > 140
-  word_substituter(tweet)[0...139] + "..."
+  new_tweet = word_substituter(tweet)[0...139] + "..."
 else
   tweet
 end
