69ad154ee6f1911520ca3b5650accdd71a05c9da
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-18 19:22:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,16 +1,25 @@
 def dictionary
 
   substitutions =
-  {"hello" => "hi",
-   "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b",
-    "you" => "u", "at" => "@", "and" => "&"}
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
 
 end
 
 def word_substituter(tweet)
   tweet.split(" ").collect do |word|
     if dictionary.keys.include?(word)
-    
+
     else
       word
     end
