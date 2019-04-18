cd85fb3bea12989ae1c583963cdc25deab2ff37f
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-18 22:33:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,12 +20,10 @@ def word_substituter(tweet)
   tweet.split(" ").collect do |word|
     if dictionary.keys.include?(word)
       word = dictionary[word]
-
     else
       word
-
     end
-    tweet.join(" ")
+    puts tweet.join(" ")
   end
 end
 
